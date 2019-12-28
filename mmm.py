a9f7b3978d3793640daaf03be35773bff12f1dfe
Alpspaca <58396693+Alspspaca@users.noreply.github.com>
2019-12-28 12:43:55 +0900

    Update mmm.py

--- mmm.py
+++ mmm.py
@@ -215,6 +215,7 @@ async def on_message(message):
         imf = soup.select('#app > section:nth-child(6) > div > div.col-lg-8.col-xl-9 > section > div > div')
         count=0
         embed = discord.Embed(color=0xFF5E00, title="경뿌 검색기")
+        await message.channel.send(embed=embed)
         for i in imf:
             text = i.text.replace('\n', '').replace(' ', '')
             if "경뿌" in text:
